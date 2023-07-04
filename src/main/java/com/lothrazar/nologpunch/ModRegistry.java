784b865a567112a15a46291361bc0bbf184e8abe
lothrazar <samson.bassett@gmail.com>
2023-07-04 07:06:22 -0700

    1.20.1 ported

--- src/main/java/com/lothrazar/nologpunch/ModRegistry.java
+++ src/main/java/com/lothrazar/nologpunch/ModRegistry.java
@@ -2,7 +2,7 @@ package com.lothrazar.nologpunch;
 
 import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.world.item.Item;
-import net.minecraftforge.event.CreativeModeTabEvent;
+import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.registries.DeferredRegister;
@@ -15,8 +15,9 @@ public class ModRegistry {
   public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, ModMain.MODID);
 
   @SubscribeEvent
-  public static void addCreative(CreativeModeTabEvent.BuildContents event) {
-    if (event.getTab() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+  public void buildContents(BuildCreativeModeTabContentsEvent event) {
+    // Add to ingredients tab
+    if (event.getTabKey() == CreativeModeTabs.TOOLS_AND_UTILITIES) {
       event.accept(FLINT_TOOL.get());
     }
   }
